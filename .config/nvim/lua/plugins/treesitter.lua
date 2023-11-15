af208bd3de1203795743db74e661fa2f9485f9ba
Max Thomson <git@mnt.dev>
2023-11-14 19:19:19 -0800

    [NVim] Add treesitter context

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,36 +1,55 @@
 return {
-	{
-        "nvim-treesitter/nvim-treesitter",
-        version = false, -- last release is way too old and doesn't work on Windows
-        build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" },
-        cmd = { "TSUpdateSync" },
-        opts = {
-            highlight = { enable = true },
-            indent = { enable = true },
-            ensure_installed = {
-                "bash",
-                "c",
-                "html",
-                "http",
-                "javascript",
-                "jsdoc",
-                "json",
-                "lua",
-                "luadoc",
-                "luap",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "yaml",
-                "help",
-            },
-        }
-    }
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSUpdateSync" },
+    -- Hack: https://github.com/virchau13/tree-sitter-astro/issues/16#issuecomment-1627752052
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {
+          "astro",
+          "bash",
+          "c",
+          "css",
+          "html",
+          "http",
+          "javascript",
+          "jsdoc",
+          "json",
+          "lua",
+          "luadoc",
+          "luap",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "regex",
+          "rust",
+          "tsx",
+          "typescript",
+          "tsx",
+          "vim",
+          "vimdoc",
+          "yaml",
+        },
+      })
+    end,
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = { "BufReadPost", "BufNewFile" },
+    version = false,
+    dependencies = {"nvim-treesitter/nvim-treesitter"},
+    config = function()
+      require('treesitter-context').setup({
+        enable = true,
+        max_lines = 3, -- How many lines the window should span. Values <= 0 mean no limit.
+      })
+    end,
+  }
 }
