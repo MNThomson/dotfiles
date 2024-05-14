fa2549d5afd0dde97abe105de79f32902301fec5
Max Thomson <git@mnt.dev>
2024-05-14 08:26:00 -0700

    [nvim] Add markdown preview

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -1,36 +1,43 @@
 return {
-    {
-        "rest-nvim/rest.nvim",
-        requires = { "nvim-lua/plenary.nvim" },
-        opts = {
-            result_split_horizontal = true,
-            result_split_in_place = true
-        },
-        config = function()
-            vim.api.nvim_create_user_command('Restrun', require('rest-nvim').run, {})
-        end
+  {
+    "rest-nvim/rest.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    opts = {
+      result_split_horizontal = true,
+      result_split_in_place = true
     },
+    config = function()
+      vim.api.nvim_create_user_command('Restrun', require('rest-nvim').run, {})
+    end
+  },
 
-    {
-        "ThePrimeagen/harpoon",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        config = true,
-        keys = {
-            { "<leader>h",  desc = "+harpoon" },
-            { "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Mark file with harpoon" },
-            { "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>",          desc = "Go to next harpoon mark" },
-            { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>",          desc = "Go to previous harpoon mark" },
-            { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
-            { "<leader>hf", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",         desc = "Go to 1st harpoon mark" },
-            { "<leader>hd", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",         desc = "Go to 2nd harpoon mark" },
-            { "<leader>hs", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",         desc = "Go to 3rd harpoon mark" },
-            { "<leader>ha", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",         desc = "Go to 4th harpoon mark" },
-            { "<leader>hv", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>",         desc = "Go to 5th harpoon mark" },
-            { "<leader>hc", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>",         desc = "Go to 6th harpoon mark" },
-            { "<leader>hx", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>",         desc = "Go to 7th harpoon mark" },
-            { "<leader>hz", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>",         desc = "Go to 8th harpoon mark" },
-        }
+  {
+    "ThePrimeagen/harpoon",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = true,
+    keys = {
+      { "<leader>h",  desc = "+harpoon" },
+      { "<leader>hm", "<cmd>lua require('harpoon.mark').add_file()<cr>",        desc = "Mark file with harpoon" },
+      { "<leader>hn", "<cmd>lua require('harpoon.ui').nav_next()<cr>",          desc = "Go to next harpoon mark" },
+      { "<leader>hp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>",          desc = "Go to previous harpoon mark" },
+      { "<leader>hh", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Show harpoon marks" },
+      { "<leader>hf", "<cmd>lua require('harpoon.ui').nav_file(1)<cr>",         desc = "Go to 1st harpoon mark" },
+      { "<leader>hd", "<cmd>lua require('harpoon.ui').nav_file(2)<cr>",         desc = "Go to 2nd harpoon mark" },
+      { "<leader>hs", "<cmd>lua require('harpoon.ui').nav_file(3)<cr>",         desc = "Go to 3rd harpoon mark" },
+      { "<leader>ha", "<cmd>lua require('harpoon.ui').nav_file(4)<cr>",         desc = "Go to 4th harpoon mark" },
+      { "<leader>hv", "<cmd>lua require('harpoon.ui').nav_file(5)<cr>",         desc = "Go to 5th harpoon mark" },
+      { "<leader>hc", "<cmd>lua require('harpoon.ui').nav_file(6)<cr>",         desc = "Go to 6th harpoon mark" },
+      { "<leader>hx", "<cmd>lua require('harpoon.ui').nav_file(7)<cr>",         desc = "Go to 7th harpoon mark" },
+      { "<leader>hz", "<cmd>lua require('harpoon.ui').nav_file(8)<cr>",         desc = "Go to 8th harpoon mark" },
     }
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  }
 }
--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -1,95 +1,100 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        cmd = "Telescope",
-        version = false, -- telescope did only one release, so use HEAD for now
-        keys = {
-            { "<leader>f",  desc = "+file" },
-            { "<leader>ff", "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files<cr>", desc = "Find Files (root dir)" },
-            { "<leader>fe", "<cmd>Explore<cr>",                                                        desc = "Explore (Netrw)" },
-            { "<leader>fg", "<cmd>Telescope live_grep<cr>",                                            desc = "Live Grep (root dir)" },
-            { "<leader>gs", "<cmd>Telescope git_status<CR>",                                           desc = "status" },
-            { "<leader>gc", "<cmd>Telescope git_commits<CR>",                                          desc = "commits" },
-        },
-        config = function()
-            require('telescope').setup {
-                defaults = {
-                    vimgrep_arguments = {
-                        'rg',
-                        '--color=never',
-                        '--no-heading',
-                        '--with-filename',
-                        '--line-number',
-                        '--column',
-                        '--smart-case',
-                        '--hidden',
-                        '--ignore-vcs',
-                    },
-                    theme = "center",
-                    sorting_strategy = "ascending",
-                    border_chars = {},
-                    layout_config = {
-                        horizontal = {
-                            prompt_position = "top",
-                        }
-                    }
-                }
+  {
+    "nvim-telescope/telescope.nvim",
+    cmd = "Telescope",
+    version = false, -- telescope did only one release, so use HEAD for now
+    keys = {
+      { "<leader>f",  desc = "+file" },
+      { "<leader>ff", "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files<cr>", desc = "Find Files (root dir)" },
+      { "<leader>fe", "<cmd>Explore<cr>",                                                        desc = "Explore (Netrw)" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>",                                            desc = "Live Grep (root dir)" },
+      { "<leader>gs", "<cmd>Telescope git_status<CR>",                                           desc = "status" },
+      { "<leader>gc", "<cmd>Telescope git_commits<CR>",                                          desc = "commits" },
+    },
+    config = function()
+      require('telescope').setup {
+        defaults = {
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+            '--ignore-vcs',
+          },
+          theme = "center",
+          sorting_strategy = "ascending",
+          border_chars = {},
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
             }
-        end,
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
+          }
+        }
+      }
+    end,
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
 
-    {
-        "lewis6991/gitsigns.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        opts = {
-            signs = {
-                add = { text = "▎" },
-                change = { text = "▎" },
-                delete = { text = "" },
-                topdelete = { text = "" },
-                changedelete = { text = "▎" },
-                untracked = { text = "▎" },
-            },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+    },
+    keys = {
+      { "<leader>g",   desc = "+git" },
+      { "<leader>gb",  "<cmd>Gitsigns blame_line<CR>",      desc = "blame line" },
+      { "<leader>gl",  "<cmd>Gitsigns setloclist<CR>",      desc = "show jump list" },
+      { "<leader>gh",  desc = "+hunk" },
+      { "<leader>ghs", "<cmd>Gitsigns stage_hunk<CR>",      desc = "stage hunk" },
+      { "<leader>ghu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "unstage hunk" },
+      { "<leader>ghr", "<cmd>Gitsigns reset_hunk<CR>",      desc = "reset hunk" },
+      { "<leader>ghn", "<cmd>Gitsigns next_hunk<CR>",       desc = "next hunk" },
+      { "<leader>ghp", "<cmd>Gitsigns prev_hunk<CR>",       desc = "next hunk" },
+    }
+  },
+
+  {
+    "folke/todo-comments.nvim",
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    event = { "BufReadPost", "BufNewFile" },
+  },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = {
+      theme = 'gruvbox',
+      sections = {
+        lualine_c = {
+          { 'filename', path = 1 }
         },
-        keys = {
-            { "<leader>g",   desc = "+git" },
-            { "<leader>gh",  desc = "+hunk" },
-            { "<leader>ghs", "<cmd>Gitsigns stage_hunk<CR>",      desc = "stage hunk" },
-            { "<leader>ghu", "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "unstage hunk" },
-        }
-    },
+      },
+    }
+  },
 
-    {
-        "folke/todo-comments.nvim",
-        cmd = { "TodoTrouble", "TodoTelescope" },
-        event = { "BufReadPost", "BufNewFile" },
-    },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {}
+  },
 
-    {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        opts = {
-            theme = 'gruvbox',
-            sections = {
-                lualine_c = {
-                    { 'filename', path = 1 }
-                },
-            },
-        }
-    },
-
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {}
-    },
-
-    --[[{
+  --[[{
         "lukas-reineke/indent-blankline.nvim",
         main = "ibl",
         opts = {
