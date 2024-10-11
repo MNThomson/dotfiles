8363b0a1f43abeb651db13e0e8d8ddd469ad94b7
Max Thomson <git@mnt.dev>
2024-10-10 17:32:51 -0700

    [nvim] Remove rest.nvim

--- .config/nvim/lua/plugins/dev.lua
+++ .config/nvim/lua/plugins/dev.lua
@@ -1,17 +1,5 @@
 return {
   {
-    "rest-nvim/rest.nvim",
-    requires = { "nvim-lua/plenary.nvim" },
-    opts = {
-      result_split_horizontal = true,
-      result_split_in_place = true
-    },
-    config = function()
-      vim.api.nvim_create_user_command('Restrun', require('rest-nvim').run, {})
-    end
-  },
-
-  {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = {
