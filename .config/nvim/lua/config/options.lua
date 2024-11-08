d0cb7e17dbfa2399cb8910616df03bff26f369c9
Max Thomson <git@mnt.dev>
2024-11-07 16:03:54 -0800

    [nvim] set scrolloff=3

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -10,15 +10,16 @@ vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
 
 local opt = vim.opt
 
-opt.expandtab = true      -- Use spaces instead of tabs
-opt.tabstop = 4           -- Number of spaces tabs count for
-opt.shiftwidth = 4        -- Size of an indent
-opt.smartcase = true      -- Don't ignore case with capitals
-opt.smartindent = true    -- Insert indents automatically
+opt.expandtab = true   -- Use spaces instead of tabs
+opt.tabstop = 4        -- Number of spaces tabs count for
+opt.shiftwidth = 4     -- Size of an indent
+opt.smartcase = true   -- Don't ignore case with capitals
+opt.smartindent = true -- Insert indents automatically
+opt.scrolloff = 3
 opt.cmdheight = 0         -- Hide the command bar by default
 
 opt.number = true         -- Print line number
 opt.relativenumber = true -- Relative line numbers
-opt.mouse = ""            -- Enable mouse mode
+opt.mouse = ""           -- Enable mouse mode
 
 vim.fn.matchadd('errorMsg', [[\s\+$]])
